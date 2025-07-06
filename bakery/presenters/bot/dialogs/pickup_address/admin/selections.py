2750f27a3d69c7e06d098cc174c89dff8968e010
Ertiops <straxisrule@gmail.com>
2025-07-06 17:56:17 +0300

    Added pickup address management

--- bakery/presenters/bot/dialogs/pickup_address/admin/selections.py
+++
@@ -1,7 +0,0 @@
-# async def on_category_selected(
-#     callback: CallbackQuery, widget: Select, manager: DialogManager, item_id: str
-# ) -> None:
-#     await manager.start(
-#         state=AdminCatalogue.view_products,
-#         data={"category": item_id},
-#     )
