50fc5d2e2439156b1bf1e9c0ee30e75006c8d1ba
Elijah Tomin <104155685+Ertiops@users.noreply.github.com>
2025-11-03 23:22:45 +0300

    fixed user card zero quantity (#17)
    Co-authored-by: Eljan <eljan@MacBook-Air-Eljan.local>

--- bakery/presenters/bot/keyboards/main_menu.py
+++ bakery/presenters/bot/keyboards/main_menu.py
@@ -11,7 +11,6 @@ def get_user_main_menu_kb() -> ReplyKeyboardMarkup:
                 KeyboardButton(text=user_menu_btn.MAKE_ORDER),
                 KeyboardButton(text=user_menu_btn.CATALOGUE),
                 KeyboardButton(text=user_menu_btn.MY_ORDERS),
-                KeyboardButton(text=user_menu_btn.HELP),
             ],
         ],
         resize_keyboard=True,
